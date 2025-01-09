e73e052c04990c788fabd7f7e05f09fdd3963575
Anna Sas <anna.sas.dev@gmail.com>
2025-01-09 16:04:50 +0100

    VersionBump : v1.1.1

--- src/CodeOfChaos.GeneratorTools/NullableAttribute.cs
+++ src/CodeOfChaos.GeneratorTools/NullableAttribute.cs
@@ -13,7 +13,7 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable UnusedType.Global
 namespace System.Diagnostics.CodeAnalysis;
-#if !NETSTANDARD2_1
+#if NETSTANDARD2_0
 
 /// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property)]
