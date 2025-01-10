411454dd16be12036b2722cced707c5770f93c15
Anna Sas <anna.sas.dev@gmail.com>
2025-01-10 18:22:06 +0100

    Fix: Deep searching of inherits from

--- src/CodeOfChaos.GeneratorTools/Extensions/NamedTypeSymbolExtensions.cs
+++ src/CodeOfChaos.GeneratorTools/Extensions/NamedTypeSymbolExtensions.cs
@@ -2,6 +2,7 @@
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
 using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
 
 namespace CodeOfChaos.GeneratorTools;
 
@@ -10,10 +11,14 @@ namespace CodeOfChaos.GeneratorTools;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class NamedTypeSymbolExtensions {
     public static bool InheritsFrom(this INamedTypeSymbol symbol, INamedTypeSymbol baseType) {
-        INamedTypeSymbol? currentSymbol = symbol;
-        while (currentSymbol is not null) {
+        Stack<INamedTypeSymbol> stack = new();
+        stack.Push(symbol);
+        
+        while (stack.TryPop(out INamedTypeSymbol? currentSymbol)) {
             if (SymbolEqualityComparer.Default.Equals(currentSymbol, baseType)) return true;
-            currentSymbol = currentSymbol.BaseType;
+            foreach (INamedTypeSymbol @interface in currentSymbol.AllInterfaces) stack.Push(@interface);
+            if (currentSymbol.BaseType == null) continue;
+            stack.Push(currentSymbol.BaseType);
         }
         return false;
     }
