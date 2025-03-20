0a0a0954302e49cb9141d8b3f4c9ec9b24972aae
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-20 10:22:55 +0100

    Feat: StringExtensions

---
+++ src/CodeOfChaos.GeneratorTools/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using System.Diagnostics.CodeAnalysis;
+
+namespace CodeOfChaos.GeneratorTools;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class StringExtensions {
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? str)
+        => string.IsNullOrEmpty(str);
+
+    public static bool IsNotNullOrEmpty([NotNullWhen(true)] this string? str)
+        => !string.IsNullOrEmpty(str);
+
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? str)
+        => string.IsNullOrWhiteSpace(str);
+
+    public static bool IsNotNullOrWhiteSpace([NotNullWhen(true)] this string? str)
+        => !string.IsNullOrWhiteSpace(str);
+
+    public static string Truncate(this string input, int maxLength) => input.Length <= maxLength ? input : input[..maxLength];
+}
