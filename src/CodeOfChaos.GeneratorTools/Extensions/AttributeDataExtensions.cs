3c39c276c788a0cc27a11cf7eb0b442a2462626b
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-09 12:08:41 +0200

    Feat: Add extension methods for `AttributeData` and `ISymbol`

---
+++ src/CodeOfChaos.GeneratorTools/Extensions/AttributeDataExtensions.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+// ReSharper disable once CheckNamespace
+namespace Microsoft.CodeAnalysis;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class AttributeDataExtensions {
+    public static bool IsDisplayName(this AttributeData attribute, string expected) {
+        return attribute.AttributeClass?.IsDisplayName(expected) ?? false;
+    }
+}
---
+++ src/CodeOfChaos.GeneratorTools/Extensions/ISymbolExtensions.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+// ReSharper disable once CheckNamespace
+namespace Microsoft.CodeAnalysis;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+// ReSharper disable once InconsistentNaming
+public static class ISymbolExtensions {
+    public static bool IsDisplayName<TSymbol>(this TSymbol? symbol, string expected) where TSymbol : ISymbol {
+        if (symbol == null) return false;
+        return symbol.ToDisplayString() == expected;
+    }
+}
