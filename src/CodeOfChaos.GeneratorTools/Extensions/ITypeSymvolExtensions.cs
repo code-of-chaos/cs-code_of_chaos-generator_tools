cc035619b334bd5788454e4f360dfe5b016cfeb2
Anna Sas <anna.sas.dev@gmail.com>
2025-05-15 15:55:12 +0200

    Add ITypeSymbolExtensions with interface display name check

---
+++ src/CodeOfChaos.GeneratorTools/Extensions/ITypeSymvolExtensions.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace CodeOfChaos.GeneratorTools;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class ITypeSymbolExtensions {
+    public static bool HasInterfaceWithDisplayName<TSymbol>(this TSymbol symbol, string displayName) where TSymbol : ITypeSymbol {
+        return symbol.AllInterfaces.Any(i => i.IsDisplayName(displayName));
+    }
+}
