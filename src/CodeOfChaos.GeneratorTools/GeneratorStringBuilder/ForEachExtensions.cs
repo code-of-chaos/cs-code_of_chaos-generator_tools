17f10ab2310877c7e867dd2ebcb173c2032b5512
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-23 18:51:54 +0200

    Feat: Add ForEachIndented overloads to support indented iteration

--- src/CodeOfChaos.GeneratorTools/GeneratorStringBuilder/ForEachExtensions.cs
+++ src/CodeOfChaos.GeneratorTools/GeneratorStringBuilder/ForEachExtensions.cs
@@ -44,4 +44,49 @@ public static class ForEachExtensions {
         foreach (T item in items) itemFormatter(builder, item, arg1, arg2);
         return builder;
     }
+    
+    public static GeneratorStringBuilder ForEachIndented<T>(
+        this GeneratorStringBuilder builder, 
+        IEnumerable<T> items,
+        Action<GeneratorStringBuilder, T> itemFormatter
+    ) {
+        if (items is ICollection<T> { Count: 0 }) return builder; // Skip iteration if no items
+
+        builder.Indent(b => {
+            foreach (T item in items) itemFormatter(b, item);
+        });
+        
+        return builder;
+    }
+
+    public static GeneratorStringBuilder ForEachIndented<T, T1>(
+        this GeneratorStringBuilder builder,
+        IEnumerable<T> items,
+        Action<GeneratorStringBuilder, T, T1> itemFormatter,
+        T1 arg
+    ) {
+        if (items is ICollection<T> { Count: 0 }) return builder; // Skip iteration if no items
+        
+        builder.Indent(b => {
+            foreach (T item in items) itemFormatter(builder, item, arg);
+        });
+        
+        return builder;
+    }
+    
+    public static GeneratorStringBuilder ForEachIndented<T, T1, T2>(
+        this GeneratorStringBuilder builder,
+        IEnumerable<T> items,
+        Action<GeneratorStringBuilder, T, T1, T2> itemFormatter,
+        T1 arg1,
+        T2 arg2
+    ) {
+        if (items is ICollection<T> { Count: 0 }) return builder; // Skip iteration if no items
+        
+        builder.Indent(b => {
+            foreach (T item in items) itemFormatter(builder, item, arg1, arg2);
+        });
+
+        return builder;
+    }
 }
