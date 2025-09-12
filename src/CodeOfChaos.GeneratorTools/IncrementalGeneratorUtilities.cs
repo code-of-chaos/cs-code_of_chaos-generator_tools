314fde263bfcc212b99376ebc93ff45b951e325f
Anna Sas <anna.sas.dev@gmail.com>
2025-09-12 13:57:28 +0200

    Add JetBrains.Annotations usage and IncrementalGeneratorUtilities class

---
+++ src/CodeOfChaos.GeneratorTools/IncrementalGeneratorUtilities.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using System;
+
+namespace CodeOfChaos.GeneratorTools;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class IncrementalGeneratorUtilities {
+    [UsedImplicitly] 
+    public static IncrementalValueProvider<string> GetAssemblyNamePipeline(IncrementalGeneratorInitializationContext context)
+        => context.CompilationProvider.Select(static (compilation, _) => compilation.AssemblyName!);
+
+    [UsedImplicitly] 
+    public static IncrementalValuesProvider<AdditionalText> GetRazorFilesPipeline(IncrementalGeneratorInitializationContext context)
+        => context.AdditionalTextsProvider.Where(static text => text.Path.EndsWith(".razor", StringComparison.OrdinalIgnoreCase));
+    
+}
--- src/CodeOfChaos.GeneratorTools/SymbolResolver.cs
+++ src/CodeOfChaos.GeneratorTools/SymbolResolver.cs
@@ -1,6 +1,7 @@
 ﻿// ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
+using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 using System.Diagnostics.CodeAnalysis;
 
@@ -10,13 +11,16 @@ namespace CodeOfChaos.GeneratorTools;
 // ---------------------------------------------------------------------------------------------------------------------
 // Simple wrapper to make testing not a living hell.
 public class SymbolResolver(SemanticModel model) {
+    [UsedImplicitly]
     public ISymbol? ResolveSymbol<TSyntaxNode>(TSyntaxNode node) where TSyntaxNode : SyntaxNode => model.GetSymbolInfo(node).Symbol;
 
+    [UsedImplicitly]
     public bool TryResolveSymbol<TSyntaxNode>(TSyntaxNode node, [NotNullWhen(true)] out ISymbol? symbol) where TSyntaxNode : SyntaxNode {
         symbol = ResolveSymbol(node);
         return symbol != null;
     }
 
+    [UsedImplicitly]
     public bool TryResolveSymbol<TSyntaxNode, TSymbol>(TSyntaxNode node, [NotNullWhen(true)] out TSymbol? symbol) where TSyntaxNode : SyntaxNode where TSymbol : class, ISymbol {
         ISymbol? resolved = ResolveSymbol(node);
         symbol = resolved as TSymbol;
