6be981b3cb3fe96ac3ead6a9a162805da6417422
Anna Sas <anna.sas.dev@gmail.com>
2025-09-12 15:24:15 +0200

    Add LocationInfo helper class to GeneratorTools

---
+++ src/CodeOfChaos.GeneratorTools/LocationInfo.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CodeOfChaos.GeneratorTools;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public sealed class LocationInfo {
+    public string FilePath { get; private set; } = null!;
+    public TextSpan TextSpan { get; private set; }
+    public LinePositionSpan LinePositionSpan { get; private set; }
+    // -----------------------------------------------------------------------------------------------------------------
+    // Constructors
+    // -----------------------------------------------------------------------------------------------------------------
+    public static LocationInfo? From<T>(T node) where T : SyntaxNode 
+        => From(node.GetLocation());
+    
+    public static LocationInfo? From(Location location) {
+        if (location.SourceTree is null) return null;
+        
+        string filePath = location.SourceTree.FilePath;
+        TextSpan span = location.SourceSpan;
+        LinePositionSpan lineSpan = location.GetLineSpan().Span;
+        
+        return new LocationInfo() {
+            FilePath = filePath,
+            TextSpan = span,
+            LinePositionSpan = lineSpan,
+        };
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public Location ToLocation()
+        => Location.Create(FilePath, TextSpan, LinePositionSpan);
+    
+}
+
